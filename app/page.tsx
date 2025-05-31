82befbcd60327b2d46f866dc6ee8af8090e42bd4
pavanscales <pawanpediredla@gmail.com>
2025-05-31 15:08:54 +0530

    refactored

--- app/page.tsx
+++ app/page.tsx
@@ -172,7 +172,8 @@ export default function Landingpage() {
                 )}
               </Button>
             </div>
-            <div className="flex items-center justify-between px-6 py-3 >
+
+            <div className="flex items-center justify-between px-6 py-3 ">
               <div className="text-sm text-gray-400">5 free messages left today.</div>
             </div>
           </Card>
